de972bc0297fe946c4a52c0acc7206bf7532d383
alfimois <alfimois@gmail.com>
2020-10-03 20:00:50 +0300

    Changed build settings

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname),
+    filename: 'bundle.[hash].js'
   },
   plugins: [new HtmlWebpackPlugin({
     template: "./src/index.html"
