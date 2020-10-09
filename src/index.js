0b8b3f3578de579e706a8ad1acd1bca9cef38941
Agito <gvagitoblgv@gmail.com>
2020-10-09 16:10:13 +0300

    Added logic when starting a program with empty fields

--- src/index.js
+++ src/index.js
@@ -6,3 +6,40 @@ const view = new View();
 const model = new Model();
 const controller = new Controller({ view, model });
 
+const encryptBtn = document.querySelector(".enc-btn");
+const messageInp = document.querySelector("#text");
+const passwordInp = document.querySelector("#password");
+const sizeInp = document.querySelector("#size");
+const formMessage = document.querySelector(".form__message");
+const formPassword = document.querySelector(".form__password");
+
+encryptBtn.addEventListener("click", () => {
+  switch(true) {
+    case messageInp.value.length === 0 && passwordInp.value.length == 0:
+      formMessage.style.borderBottomColor = '#FF0000';
+      formPassword.style.borderBottomColor = '#FF0000';
+    break;
+
+    case messageInp.value.length === 0:
+      formMessage.style.borderBottomColor = '#FF0000';
+    break;
+    
+    case passwordInp.value.length == 0:
+      formPassword.style.borderBottomColor = '#FF0000';
+    break;
+
+    default:
+      messageInp.value = "";
+      passwordInp.value = "";
+      sizeInp.value = "";
+  }
+});
+
+function changeColorBorder(input, form) {
+  input.addEventListener("focus", () => {
+    form.style.borderBottomColor = '#8f8f8f';
+  });
+}
+
+changeColorBorder(messageInp, formMessage);
+changeColorBorder(passwordInp, formPassword);
